6da253d8e516478d075bd53b91099f0ed214dec6
Colin King <colin@hakuj.in>
2026-08-15 18:49:45 -0500

    Bump to 0.46

--- package.nix
+++ package.nix
@@ -17,11 +17,11 @@ let custom-chez = chez.overrideAttrs (_: prev: {
 });
 in stdenv.mkDerivation (final: {
   pname = "shen-scheme";
-  version = "0.45";
+  version = "0.46";
 
   src = builtins.fetchTarball {
     url = "https://github.com/tizoc/shen-scheme/releases/download/v${final.version}/shen-scheme-v${final.version}-src.tar.gz";
-    sha256 = "sha256:072jgm0nv4l2cbnn67rh6vc746r5dgfnymkl4yjvfxb9r1dini53";
+    sha256 = "sha256:1mfikhl03xmavwd2yqpskj7faz4lpg9kwkykr87m0870dn8g5ln6";
   };
 
   nativeBuildInputs = [ custom-chez lz4 zlib ];
